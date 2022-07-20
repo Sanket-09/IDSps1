e21c572b05cb8d418e833db14e2852c13bcce740
Abhijay Tyagi <abhijaytyagi998@gmail.com>
2022-07-21 01:04:13 +0530

    Changes by Abhijay-NGO

--- IDSps1/Controllers/NGOController.cs
+++ IDSps1/Controllers/NGOController.cs
@@ -18,6 +18,23 @@ namespace IDSps1.Controllers
             IEnumerable<Review> objReviews = _db.Reviews;
             return View(objReviews);
         }
-        
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Name,Requirements,Link")] Review Reviews)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Add(Reviews);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(Reviews);
+        }
     }
 }
